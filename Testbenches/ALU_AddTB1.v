0cc9c2081796e72ca2dea2e032abeac966a5bc11
Jack <jstudiner44@gmail.com>
2022-02-15 22:32:51 -0500

    got a simple testbench for the add signal working

--- Testbenches/ALU_AddTB1.v
+++ Testbenches/ALU_AddTB1.v
@@ -1,19 +1,28 @@
-
-module RCAdd_tb; 
-reg [7:0] in_a;
-reg [7:0] in_b;  //A and B
+`timescale 1ns/10ps
+module ALU_AddTB1(
+input wire A,
+output reg B
+); 
+reg [31:0] in_a;
+reg clk;
+reg [31:0] in_b;  //A and B
 reg [4:0] in_c;  // select
 wire [7:0] out_res_rc; // Output 
 wire [7:0] Carryout;
 
-ALU(in_a, in_b, in_c, out_res_rc, Carryout);
-
-initial begin
+ALU test(in_a, in_b, in_c, out_res_rc, Carryout);
+initial 
+begin
+       clk = 0;  
+end
+always
+#10 clk = ~ clk; 
+always@(posedge clk)
+	begin
+	 B=1;
     in_a <= 5;
     in_b <= 2;
     in_c <= 0;
+	end
 
-
-end 
-RCAdd ALU(in_a, in_b,in_c ,out_res_rc,Carryout); 
 endmodule 
