7150f721b4ef820816a4334f7fcec86a6fc99962
Jack <jstudiner44@gmail.com>
2022-03-03 15:10:58 -0500

    demo worked baby

--- datapath.v
+++ datapath.v
@@ -18,7 +18,9 @@ module datapath(
     input wire [31:0] Mdatain,
     input wire read,
     input wire clr,
-    input wire [31:0] reg_enable
+    input wire [31:0] reg_enable,
+    input wire incPC
+
 );
     reg [31:0] MDR_Out;
     wire [4:0] S;
@@ -31,12 +33,13 @@ module datapath(
     //MDR stuff
     wire [31:0] MdMUXout;
     mux_2_to_1 MDMUX(MdMUXout,bus_contents,Mdatain,read);
-     reg MDR_enable;
     reg_32bit MDR(clk,clr,reg_enable[22],MdMUXout,MDR_data_out);
-    reg Y_enable;
+    //ALU stuff
     wire [31:0] ALU_Low_Out,ALU_High_Out;
     wire ALU_carry_out;
     ALU alu(Y_data_out,bus_contents,ALU_Sel,Zlow_data_out,Zhigh_data_out,ALU_carry_out,clk);
+    //PC reg
+    PC_reg PC(clk, clr, reg_enable[20], incPC, bus_contents, PC_data_out);
     reg_32bit R0(clk, clr, reg_enable[0], bus_contents, r0_data_out); // data out is bus input
     reg_32bit R1(clk, clr, reg_enable[1], bus_contents, r1_data_out);
     reg_32bit R2(clk, clr, reg_enable[2], bus_contents, r2_data_out);
@@ -44,7 +47,7 @@ module datapath(
     reg_32bit R4(clk, clr, reg_enable[4], bus_contents, r4_data_out);
     reg_32bit R5(clk, clr, reg_enable[5], bus_contents, r5_data_out);
     reg_32bit R6(clk, clr, reg_enable[6], bus_contents, r6_data_out);
-    reg_32bit R7(clk, clr, reg_enable[7], bus_contents, r7_data_out);
+    reg_32bit R7(clk, clr, reg_enable[7], bus_contents, r7_data_ out);
     reg_32bit R8(clk, clr, reg_enable[8], bus_contents, r8_data_out);
     reg_32bit R9(clk, clr, reg_enable[9], bus_contents, r9_data_out);
     reg_32bit R10(clk, clr, reg_enable[10], bus_contents, r10_data_out);
@@ -57,7 +60,7 @@ module datapath(
     reg_32bit LO(clk, clr, reg_enable[17], bus_contents, LO_data_out);
     reg_32bit Zhigh(clk, clr, reg_enable[18], ALU_High_Out, Zhigh_data_out);
     reg_32bit Zlow(clk, clr, reg_enable[19], ALU_Low_Out, Zlow_data_out);
-    reg_32bit PC(clk, clr, reg_enable[20], bus_contents, PC_data_out);
+    
     reg_32bit IR(clk, clr,reg_enable[21], bus_contents, IR_data_out);
     reg_32bit MAR(clk, clr, reg_enable[23], bus_contents, MAR_data_out);
     reg_32bit Y(clk, clr,reg_enable[24], bus_contents, Y_data_out);
